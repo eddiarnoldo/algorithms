577f543aca7ee97b08eabccf767c8e606de97946
Eduardo Quintana Chavez <eduardoquintanachavez@Eduardos-MacBook-Pro.local>
2021-10-17 23:04:21 -0600

    Decode string

---
+++ src/main/java/medium/DecodeString.java
@@ -0,0 +1,57 @@
+package medium;
+
+import java.util.Stack;
+
+public class DecodeString {
+
+    /*
+        Receive a string like:
+         - "5[a]2[b]" -> aaaaabb
+         - "2[3[c]2[t]]" -> cccttccctt
+     */
+
+    public static void main(String[] args) {
+        System.out.println(decode("5[a]2[b]"));
+        System.out.println(decode("2[3[c]2[t]]"));
+        System.out.println(decode("3[a2[c]]"));
+    }
+
+    public static String decode(String s) {
+        Stack<String> results = new Stack<>();
+        Stack<Integer> multipliers = new Stack<>();
+        String res = "";
+        int idx = 0;
+        int length = s.length();
+
+        while (idx < length) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int num = 0;
+                multipliers.push(num);
+                while (Character.isDigit(s.charAt(idx))) {
+                    num = multipliers.pop();
+                    num = num * 10 + (s.charAt(idx) - '0');
+                    multipliers.push(num);
+                    idx++;
+                }
+            } else if (s.charAt(idx) == '[') {
+                results.push(res);
+                res = "";
+                idx++;
+            } else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder(results.pop());
+                int count = multipliers.pop();
+                for (int i = 0; i < count; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            } else {
+                res += s.charAt(idx);
+                idx++;
+            }
+        }
+
+        return res;
+    }
+
+}
